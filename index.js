a1d468d43edaac70431fe1f781aa88a9a02a1d32
Muhammed Yaseen Sidhik <mhdyaseensid@gmail.com>
2024-10-26 21:57:30 +0530

    front-end-added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+app.use(express.static("dist"));
+
 // app.use(
 //   cors({
 //     origin: "http://localhost:5173",
